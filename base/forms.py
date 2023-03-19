64a60392d8dbec0606ee51af298c79472e436973
Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>
2023-03-19 18:45:03 +0530

    regster page

--- base/forms.py
+++ base/forms.py
@@ -5,9 +5,19 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserSignUpForm(UserCreationForm):
+    
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'password1': forms.PasswordInput(attrs={'placeholder': 'Password'}),
+            'password2': forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}),
+        }
+
 
 
 class ProfileForm(ModelForm):
