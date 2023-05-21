fe3a69e4bbb7b088e86b7dbe1c32025e6a969d8e
Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>
2023-05-21 21:16:48 +0530

    DEPLOY 2

--- moodle/settings.py
+++ moodle/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '2b09-103-123-226-98.ngrok-free.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
