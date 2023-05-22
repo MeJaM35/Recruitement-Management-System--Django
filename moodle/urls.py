96bd1d13a45985d351465f3db8454ef09239a9c7
Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>
2023-05-22 16:05:52 +0530

    Update urls.py

--- moodle/urls.py
+++ moodle/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 #from . import base
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
 
@@ -33,5 +34,6 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 
 
